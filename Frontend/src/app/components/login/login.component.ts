9c078ec9729b3d625e786c2d3226958a62cb7b7e
sebasgomez16 <sebasgomez1603@gmail.com>
2024-04-30 20:20:33 -0500

    Se crea crud para login y register

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+
+  users: any[] = [];
+
+  form = new FormGroup({
+    user: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+
+  constructor(private router: Router) {
+    this.users = [
+      {name: 'superusuario', password: 'subgrupo15' }
+    ];
+
+  }
+
+  goRegister() {
+    this.router.navigate(['register/']);
+  }
+
+  getInto() {
+    let elements = this.form.value;
+    var usr = this.users.filter(name => name.name === elements.user);
+    var pass = this.users.filter(name => name.password === elements.password);
+
+    if(usr.length > 0 && pass.length > 0) {
+      this.router.navigate(['administration/']);
+    } else {
+      swal.fire('Hubo un error', `El usuario o clave son incorrectos`, 'error');
+    }
+
+  }
 
 }
